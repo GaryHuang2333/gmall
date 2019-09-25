5d6f71dfb4cfb410a98c37859f73128fc88f4619
GaryHuang <GaryHuang@gary.com>
2019-09-26 00:26:24 +0800

    init project gmall

---
+++ src/main/java/com/gary/gmall/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package com.gary.gmall.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("/admin")
+    public String adminLogin(){
+        return "admin login";
+    }
+
+    @GetMapping("/customer")
+    public String customerLogin(){
+        return "customer login";
+    }
+
+    @GetMapping("/seller")
+    public String sellerLogin(){
+        return "seller login";
+    }
+}
